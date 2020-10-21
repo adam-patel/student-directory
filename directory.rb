ea325d7462a5120f41042d72db99822c9f8f0500
adam2021 <adam.patel101@outlook.com>
2020-10-21 14:11:15 +0100

    exercise 9 - grammar

--- directory.rb
+++ directory.rb
@@ -9,7 +9,11 @@ def input_students
   while !name.empty? do
     #add the student hash to the array
     students << {name: name, cohort: :november, country: "GB", }
+    if students.count == 1
+    puts "Now we have 1 student"
+  else
     puts "Now we have #{students.count} students"
+  end
     # get another name from the user
     name = gets.chomp
   end
