3cbfd765badf06362eb83b2e8b59fe3c26a08a7f
adam2021 <adam.patel101@outlook.com>
2020-10-22 15:24:22 +0100

    ex4 - rewrite each as while

--- directory.rb
+++ directory.rb
@@ -6,9 +6,14 @@ def input_students
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
+  puts "Enter the cohort (november by default)"
+  cohort = gets.chomp.to_sym
+  if cohort.empty?
+    cohort = :november
+  end
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november, country: "GB", }
+    students << {name: name, cohort: cohort, country: "GB", }
     if students.count == 1
     puts "Now we have 1 student"
   else
@@ -16,6 +21,11 @@ def input_students
   end
     # get another name from the user
     name = gets.chomp
+    puts "Enter the cohort (november by default)"
+    cohort = gets.chomp.to_sym
+    if cohort.empty?
+      cohort = :november
+    end
   end
   #return the array of students
   students
@@ -25,21 +35,62 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
-    count = 0
-    students.each do |student|
-    puts count + 1
-    puts "#{student[:name]} (#{student[:cohort]} cohort) #{student[:country]}"
-    count = count + 1
+    cohorts = students.map{ |s| s[:cohort]}
+
+    n = 0
+    while n < students.count
+      puts "#{students[n][:name]} (#{students[n][:cohort]})"
+      n = n + 1
+  #  students.each do |student|
+  #  puts "#{student[:name]} (#{student[:cohort]} cohort) #{student[:country]}"
+    end
+end
+
+def print_list_by_letter(students)
+  puts "Names beginning with what letter?"
+  letter = gets.chomp.to_s
+  initial = students.select { |student| student[:name].start_with?(letter) }
+  initial.each do |v|
+  puts "#{v[:name]}, #{v[:cohort]}"
   end
-  end
+end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-#nothing happens until we call the methods
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Show students by initial"
+    puts "9. Exit"
+
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "3"
+      print_list_by_letter(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant. Try again."
+    end
+  end
+end
+
+# nothing happens until we call the methods
+interactive_menu
 students = input_students
 print_header
 print(students)
+print_list_by_letter(students)
 print_footer(students)
